bf51f42c703206e6df7804cd7f7808ed4c468914
Christos Panagiotakopoulos <chrispanag@gmail.com>
2022-01-17 23:02:25 +0200

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [ranking, setRanking] = React.useState<number | null>(null);
+  const [inputText, setInputText] = React.useState<string>("");
+  const [hash, setHash] = React.useState<number | null>();
+
+  const updateHash = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const parsedHash = parseInt(e.target.value);
+    if (isNaN(parsedHash)) {
+      setInputText("");
+      return
+    }
+
+    setInputText(e.target.value)
+    setHash(parsedHash)
+  }
+
+  const updateRanking = async () => {
+    if (inputText.length === 0) {
+      return;
+    }
+
+    if (hash === null) {
+      return;
+    }
+    const res = await fetch('https://api.nearnauts.io/get_mintnauts', {
+      method: 'POST',
+      body: JSON.stringify({
+        account: "naut",
+        hash: [hash]
+      }),
+      headers: {
+        'content-type': 'application/json'
+      }
+    });
+
+    const data = await res.json();
+    setInputText("")
+
+    if (data.data.length < 1) {
+      return
+    }
+
+    setRanking(data.data[0].rank)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {ranking ? <div><p>Nearnaut {'#' + hash} is of rank...</p><h1>{ranking}</h1></div> : <h1>Enter the Nearnaut id # to get its ranking...</h1>}
+        <input onChange={updateHash} value={inputText} style={{ width: '50%', height: '50px', fontSize: '1em' }} />
+        <button style={{ marginTop: '50px', width: '25%' }} className='button' onClick={() => updateRanking()}><b>Check</b></button>
       </header>
     </div>
   );
